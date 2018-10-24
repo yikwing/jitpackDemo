a4ad3d72e26fb0cde5fde9f47b44dffdce3f21fc
yikwing <hubeieasy@163.com>
2018-10-24 14:51:35 +0800

    init

---
+++ mylibrary/src/main/java/com/arkcloud/mylibrary/BaseApplication.kt
@@ -0,0 +1,32 @@
+package com.arkcloud.mylibrary
+
+import android.app.Application
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
+
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initThridPack()
+    }
+
+    private fun initThridPack() {
+        initLogger()
+    }
+
+}
+
+private fun initLogger() {
+
+    val formatStrategy = PrettyFormatStrategy
+        .newBuilder() // (Optional) Changes the log strategy to print out. Default LogCat
+        .tag("yikwing")   // (Optional) Global tag for every log. Default PRETTY_LOGGER
+        .build()
+
+    Logger.addLogAdapter(object : AndroidLogAdapter(formatStrategy) {
+        override fun isLoggable(priority: Int, tag: String?): Boolean {
+            return BuildConfig.DEBUG
+        }
+    })
+}
